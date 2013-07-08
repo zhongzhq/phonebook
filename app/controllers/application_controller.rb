7703358bc37eb405f46f116abecdc9347ebb7854
唐久军 <1549907179@qq.com>
2013-07-08 23:24:24 +0800

    配置 devise 邮件地址，设置 devise 页面主题为 default。templates/_header 判断用户是否登陆，并显示登陆信息。修改 zh_CN.devise.yml 邮件主题

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate_user!
+
   # reset captcha code after each request for security
   after_filter :reset_last_captcha_code!
-
+  
   protect_from_forgery
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "default"
+    else
+      "application"
+    end
+  end
 end
