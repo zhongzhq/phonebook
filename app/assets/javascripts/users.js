b973468dde64f3692969512edfcae4e64d1961d8
唐久军 <1549907179@qq.com>
2013-07-08 11:15:32 +0800

    修改 captcha 的请求 url

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,3 @@
 function refresh_captcha( obj ){
-  $('#captcha').next().find('img').attr('src', '/captcha?action=captcha')
+  $('#captcha').next().find('img').attr('src', '/captcha?action=captcha&i=' + new Date().getTime())
 }
