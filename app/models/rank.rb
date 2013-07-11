dc6d31916bbb1f88846d55960450edf07790e9da
唐久军 <1549907179@qq.com>
2013-07-11 14:58:20 +0800

    rank 添加 has_ancestry

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 class Rank < ActiveRecord::Base
-  attr_accessible :name, :parent_id
+  attr_accessible :name, :parent
 
   validates :name, presence: true, uniqueness: true
 
-  has_one :child, class_name: 'Rank', foreign_key: 'parent_id'
-  belongs_to :parent, class_name: 'Rank'
-
   has_many :organs
 
+  has_ancestry
 
   def self.get_childs parent_id
     result = []
--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(:version => 20130617083036) do
 
   create_table "ranks", :force => true do |t|
     t.string :name
-    t.integer :parent_id
+    t.string :ancestry
     # t.integer :level ,:defaut=>1 # 1企业级 0系统级
 
     t.datetime "created_at", :null => false
