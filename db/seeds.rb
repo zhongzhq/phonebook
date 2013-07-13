5fa6c930fe71272a14dd288af2e0e454c41245b6
唐久军 <1549907179@qq.com>
2013-07-13 19:53:11 +0800

    修改 seed, 初始化 公司所有人 数据，并激活

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,32 +20,23 @@ organ1 = Organ.create!(name: '成都知一软件有限公司', rank_id: rank1.id
   organ13 = Organ.create!(name: '行政部', rank_id: rank11.id, parent_id: organ1)
   organ14 = Organ.create!(name: '财务部', rank_id: rank11.id, parent_id: organ1 )
 
-
-
+User.create!(account: "suyu", name: "苏渝", phone: 18602881279, email: "suyu@zhiyisoft.com",password: "18602881279",password_confirmation:  "18602881279")
+User.create!(account: "xiegang", name: "谢刚", phone: 18628171676, email: "xiegang@zhiyisoft.com",password: "18628171676",password_confirmation:  "18628171676")
+User.create!(account: "zhongzhengquan", name: "钟正权", phone: 13551147353, email: "zhongzhengquan@zhiyisoft.com",password: "13551147353",password_confirmation:  "13551147353")
+User.create!(account: "yangjunfeng", name: "杨峻峰", phone: 15881151751, email: "yangjunfeng@zhiyisoft.com",password: "15881151751",password_confirmation:  "15881151751")
+User.create!(account: "chenjianbin", name: "陈健斌", phone: 15882357025, email: "chenjianbin@zhiyisoft.com",password: "15882357025",password_confirmation:  "15882357025")
+User.create!(account: "yangdan", name: "杨丹", phone: 13281283611, email: "yangdan@zhiyisoft.com",password: "13281283611",password_confirmation:  "13281283611")
+User.create!(account: "heyuan", name: "何源", phone: 13320963363, email: "heyuan@zhiyisoft.com",password: "13320963363",password_confirmation:  "13320963363")
+User.create!(account: "recurlamlisp", name: "黄德洲", phone: 18782902305, email: "recurlamlisp@gamil.com",password: "18782902305",password_confirmation:  "18782902305")
+User.create!(account: "zhangtao", name: "张涛", phone: 15202826031, email: "zhangtao@zhiyisoft.com",password: "15202826031",password_confirmation:  "15202826031")
+User.create!(account: "yinchangxin", name: "尹常鑫", phone: 15184469287, email: "yinchangxin@zhiyisoft.com",password: "15184469287",password_confirmation:  "15184469287")
+User.create!(account: "tanghao", name: "唐浩", phone: 13880129915, email: "tanghao@zhiyisoft.com",password: "13880129915",password_confirmation:  "13880129915")
+User.create!(account: "yanqiuyun", name: "闫秋云", phone: 13808229662, email: "yanqiuyun@zhiyisoft.com",password: "13808229662",password_confirmation:  "13808229662")
+User.create!(account: "liuling", name: "刘玲", phone: 13541386053, email: "liuling@zhiyisoft.com",password: "13541386053",password_confirmation:  "13541386053")
+User.create!(account: "lijia", name: "李佳", phone: 13308176710, email: "lijia@zhiyisoft.com",password: "13308176710",password_confirmation:  "13308176710")
 
 @user = User.create!(account: "tianbymy",email:"tianbymy@163.com",name:"1",password:"adminxg",password_confirmation:"adminxg",phone: "18628171676")
 @actor = Actor.create!(membership_id: membership2.id,organ_id: organ11_2.id)
 @actor.users << @user
 
-=begin
-
-User.create!(account: "suyu", name: "苏渝",membership_id: membership1.id,organ_id: organ1.id, phone: 18602881279, email: "suyu@zhiyisoft.com",password: "18602881279",password_confirmation:  "18602881279")
-User.create!(account: "xiegang", name: "谢刚",membership_id: membership2.id,organ_id: organ11.id, phone: 18628171676, email: "xiegang@zhiyisoft.com",password: "18628171676",password_confirmation:  "18628171676")
-User.create!(account: "zhongzhengquan", name: "钟正权",membership_id: membership2.id,organ_id: organ11.id, phone: 13551147353, email: "zhongzhengquan@zhiyisoft.com",password: "13551147353",password_confirmation:  "13551147353")
-User.create!(account: "yangjunfeng", name: "杨峻峰",membership_id: membership2.id,organ_id: organ11.id, phone: 15881151751, email: "yangjunfeng@zhiyisoft.com",password: "15881151751",password_confirmation:  "15881151751")
-User.create!(account: "chenjianbin", name: "陈健斌",membership_id: membership2.id,organ_id: organ11.id, phone: 15882357025, email: "chenjianbin@zhiyisoft.com",password: "15882357025",password_confirmation:  "15882357025")
-User.create!(account: "yangdan", name: "杨丹",membership_id: membership2.id,organ_id: organ11.id, phone: 13281283611, email: "yangdan@zhiyisoft.com",password: "13281283611",password_confirmation:  "13281283611")
-User.create!(account: "heyuan", name: "何源",membership_id: membership2.id,organ_id: organ11.id, phone: 13320963363, email: "heyuan@zhiyisoft.com",password: "13320963363",password_confirmation:  "13320963363")
-User.create!(account: "recurlamlisp", name: "黄德洲",membership_id: membership2.id,organ_id: organ11.id, phone: 18782902305, email: "recurlamlisp@gamil.com",password: "18782902305",password_confirmation:  "18782902305")
-User.create!(account: "zhangtao", name: "张涛",membership_id: membership2.id,organ_id: organ11.id, phone: 15202826031, email: "zhangtao@zhiyisoft.com",password: "15202826031",password_confirmation:  "15202826031")
-User.create!(account: "yinchangxin", name: "尹常鑫",membership_id: membership2.id,organ_id: organ11.id, phone: 15184469287, email: "yinchangxin@zhiyisoft.com",password: "15184469287",password_confirmation:  "15184469287")
-User.create!(account: "tanghao", name: "唐浩",membership_id: membership2.id,organ_id: organ12.id, phone: 13880129915, email: "tanghao@zhiyisoft.com",password: "13880129915",password_confirmation:  "13880129915")
-User.create!(account: "yanqiuyun", name: "闫秋云",membership_id: membership2.id,organ_id: organ13.id, phone: 13808229662, email: "yanqiuyun@zhiyisoft.com",password: "13808229662",password_confirmation:  "13808229662")
-User.create!(account: "liuling", name: "刘玲",membership_id: membership2.id,organ_id: organ14.id, phone: 13541386053, email: "liuling@zhiyisoft.com",password: "13541386053",password_confirmation:  "13541386053")
-User.create!(account: "lijia", name: "李佳",membership_id: membership2.id,organ_id: organ14.id, phone: 13308176710, email: "lijia@zhiyisoft.com",password: "13308176710",password_confirmation:  "13308176710")
-
-
-User.create!(account: "tianbymy", name: "谢刚",membership_id: membership2.id,organ_id: organ11.id, phone: 18628171676, email: "tianbymy@163.com",password: "adminxg",password_confirmation:  "adminxg")
-
-=end
-
+User.all.map { |e| e.update_attribute(:confirmation_token, nil) }
