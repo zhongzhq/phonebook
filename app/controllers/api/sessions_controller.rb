2cff4513484cd3c0d38931a374a0f804228e1382
XieGang <xiegang@zhiyisoft.com>
2013-07-15 16:13:17 +0800

    添加zysong 字体

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 class Api::SessionsController < Api::BaseController
 
-  before_filter :ensure_params_exist
-
   def login
-    resource = User.find_for_database_authentication(:email => params[:user_login][:email])
+    resource = User.find_for_database_authentication(:email => params[:account])
     return invalid_login_attempt unless resource
 
-    if resource.valid_password?(params[:user_login][:password])
+    if resource.valid_password?(params[:password])
       sign_in(:user, resource)
       resource.ensure_authentication_token!
       render :json=> {:status => "success", :auth_token => resource.authentication_token, :email=>resource.email},:callback=>params[:callback]
@@ -17,7 +15,7 @@ class Api::SessionsController < Api::BaseController
   end
 
   def logout
-    resource = User.find_for_database_authentication(:email => params[:user_login][:email])
+    resource = User.find_for_database_authentication(:email => params[:email])
     if resource && resource.update_attribute(:authentication_token, nil)
       render :json => {:status => 'success', :message => '退出成功'},:callback=>params[:callback]
     else
@@ -26,9 +24,8 @@ class Api::SessionsController < Api::BaseController
   end
 
   def get_user
-    result = User.where(email: params[:user_login][:email],authentication_token: params[:user_login][:token])
+    result = User.where(email: params[:email],authentication_token: params[:token])
     if result.first
-      p result.first
       render :json =>result.first,:callback=>params[:callback]
     else
       render :json => {:status => 'failed', :message => "请先登录"},:callback=>params[:callback]
@@ -37,10 +34,6 @@ class Api::SessionsController < Api::BaseController
   end
 
   protected
-  def ensure_params_exist
-    return unless params[:user_login].blank?
-    render :json=>{:status=>"failed", :message=>"missing user_login parameter"}, :status=>422
-  end
 
   def invalid_login_attempt
     render :json=> {:status=>"failed", :message=>"Error with your login or password"}, :status=>401
