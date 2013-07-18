fa47044f71f14015b05976e650aef7df953d05b4
唐久军 <1549907179@qq.com>
2013-07-18 13:48:42 +0800

    为 membership 和 rank 模型 添加注视说明

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -7,6 +7,14 @@ SystemRoles = {
   :system_admin => '系统管理员'
 }
 
+# Membership 用户标识 系统用户 角色
+# 角色有两种状态：
+#   当状态为 0 时，表示：
+#     普通角色（如： 经理）
+#   当状态为 1 时，表示：
+#     系统角色（如：组织成员，组织管理员，系统管理员）
+#     系统角色不能被删除，一般通过初始化方式添加
+
 class Membership < ActiveRecord::Base
   attr_accessible :name
 
@@ -16,15 +24,16 @@ class Membership < ActiveRecord::Base
 
   # 系统角色不能删除
   before_destroy {|membership| membership.status != 1}
-
-  ## 根据 SystemRoles 的键定义获取系统角色的方法
-  #
-  # 将会定义如下方法：
-  #   Membership.system_admin
-  #   Membership.organ_member
-  #   Membership.organ_admin
   
   class << self
+
+    ## 根据 SystemRoles 的键定义获取系统角色的方法
+    #
+    # 将会定义如下方法：
+    #   Membership.system_admin
+    #   Membership.organ_member
+    #   Membership.organ_admin
+    
     SystemRoles.each do |method_name, membership_name|
       define_method method_name do
         where(name: membership_name).first
--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+# Rank model 用于标识 组织 的等级
+# Rank 的 每条记录 对应 零条或一条 子记录
 class Rank < ActiveRecord::Base
   attr_accessible :name, :parent_id
 
@@ -7,6 +10,6 @@ class Rank < ActiveRecord::Base
   has_many :organs
   has_ancestry
 
-  # rank 只能有一个 children
+  # 一条 rank 记录只能有一个 children 记录
   before_save {|rank| p rank; rank.parent.try(:children).blank? }
 end
