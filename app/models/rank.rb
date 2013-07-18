b217dc71a2a0d44547206a98c1e3eebe9ffe280a
唐久军 <1549907179@qq.com>
2013-07-18 12:12:42 +0800

    验证 一个等级下只能有一个子级， 并测试

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -5,6 +5,8 @@ class Rank < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   has_many :organs
-
   has_ancestry
+
+  # rank 只能有一个 children
+  before_save {|rank| p rank; rank.parent.try(:children).blank? }
 end
--- spec/models/rank_spec.rb
+++ spec/models/rank_spec.rb
@@ -7,4 +7,8 @@ describe Rank do
     @bu_men = create :bu_men, parent: @qi_ye
     @ke_shi = create :ke_shi, parent: @bu_men
   end
+
+  it '一个等级下只能有一个子等级' do
+    expect( Rank.new( :name => '部门级别', :parent_id => @qi_ye.id ).save ).to be_false
+  end
 end
