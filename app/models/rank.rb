8db172ccc1af4330f9068d18b4ea2128cfd4f1a5
唐久军 <1549907179@qq.com>
2013-07-18 16:47:33 +0800

    删除 rank 等级 一对一 限制

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Rank model 用于标识 组织 的等级
-# Rank 的 每条记录 对应 零条或一条 子记录
+# Rank 用于标识组织的等级
+
 class Rank < ActiveRecord::Base
   attr_accessible :name, :parent_id
 
@@ -9,7 +9,4 @@ class Rank < ActiveRecord::Base
 
   has_many :organs
   has_ancestry
-
-  # 一条 rank 记录只能有一个 children 记录
-  before_save {|rank| p rank; rank.parent.try(:children).blank? }
 end
