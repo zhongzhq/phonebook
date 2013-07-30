267d19736b329faeb1cc31db7ec234c2b8384107
唐久军 <1549907179@qq.com>
2013-07-30 16:02:46 +0800

    修正登陆认证时不验证 captcha bug

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,12 +3,12 @@ class Users::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
 
   def create
-    self.resource = warden.authenticate!(auth_options)
+    @user = User.new(params[:user])
+
     if request.post? && captcha_valid?(params[:captcha])
-      @user = User.where( email: params[:user][:email] ).first
       super
     else
-      resource.errors.add(:captcha, '验证码错误')
+      @user.errors.add(:captcha, '验证码错误')
       render 'new',layout: "default"
     end
   end
