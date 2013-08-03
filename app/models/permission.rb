53ccfab18afbc7af611f93e33956ecf2a1d0833b
唐久军 <1549907179@qq.com>
2013-08-03 16:21:46 +0800

    permission model 加入测试数据

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,8 +1,9 @@
 class Permission < ActiveRecord::Base
+  # level 默认为 1，表示权限属于系统管理组
   attr_accessible :name, :code, :description, :level
 
   validates :name, :code, presence: true, :uniqueness => true
-  validates :level, :inclusion => { :in => 0..1 }
+  validates :level, :inclusion => { :in => [0, 1] }
 
   has_and_belongs_to_many :actors
 end
--- spec/factories/permissions.rb
+++ spec/factories/permissions.rb
@@ -2,5 +2,30 @@
 FactoryGirl.define do
   factory :permission do
 
+    factory :manage_all do
+      name '管理系统'
+      code 'can :manage, :all'
+    end
+
+    factory :manage_organ do
+      name '管理所属企业'
+      code '
+      can :manage, Organ do |organ|
+        user.root_organ == organ.root
+      end
+      '
+      level 0
+    end
+
+    factory :read_organ do
+      name '查看所属企业'
+      code '
+      can :read, Organ do |organ|
+        user.root_organ == organ.root
+      end
+      '
+      level 0
+    end
+
   end
 end
