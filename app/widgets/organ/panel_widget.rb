2a435c38789bc503a60fd2d9d41d9a5019074d46
唐久军 <1549907179@qq.com>
2013-08-08 22:49:25 +0800

    删除角色功能可用

--- app/widgets/organ/panel_widget.rb
+++ app/widgets/organ/panel_widget.rb
@@ -1,6 +1,8 @@
 class Organ::PanelWidget < ApplicationWidget
   responds_to_event :show
   responds_to_event :all
+  responds_to_event :remove_from_show
+  responds_to_event :remove_form_all
 
   def display args
     @organ = Organ.find args[:id]
@@ -21,15 +23,24 @@ class Organ::PanelWidget < ApplicationWidget
     replace :view => :all
   end
 
-  # 把用户从指定组织中移除
-  def remove
-    @organ = Organ.find(params[:organ_id])
+  # 删除用户的 actor
+  def remove_from_show
+    @organ = Organ.find params[:id]
+    remove @organ, :user_id => params[:user_id], :membership_id => params[:membership_id]
+    render :state => :show
+  end
 
-    User.find(params[:user_id]).actors.delete(
-      Actor.first_or_create( :organ => Organ.find(params[:organ_id]),
-        :membership => Membership.find(params[:membership_id]) )  
+  def remove_form_all
+    @organ = Organ.find params[:id]
+    remove @organ, :user_id => params[:user_id], :membership_id => params[:membership_id]
+    render :state => :all
+  end
+
+  private
+  def remove organ, args = {}
+    User.find(args[:user_id]).actors.delete(
+      Actor.first_or_create(:organ => organ, :membership => Membership.find(args[:membership_id]))
       )
-    replace view: :display
   end
 
 end
