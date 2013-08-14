d923abae818aa1e5f6063580f3d5eb6f9e4bb7da
Tianbymy <xiegang@zhiyisoft.com>
2013-08-14 16:20:52 +0800

    优化 get_users api

--- app/api/phonebook/users.rb
+++ app/api/phonebook/users.rb
@@ -4,8 +4,9 @@ module Phonebook
   class Users < Grape::API
     use Rack::JSONP
     format  :json
+
     desc "获取组织根节点所有用户"
-    get "/all_users" do
+    get "all_users" do
       root = Organ.where(id: params[:organ_id]).first
       users = root.subtree_members.order("name ASC")
       users.inject({}) do |index,user|
@@ -13,16 +14,12 @@ module Phonebook
       end
       users
     end
+
     desc "获取"
-    get "/get_users" do
+    get "get_users" do
       if params[:organ_id]
-        organ = Organ.where(id: params[:organ_id]).first
-        actors = Actor.where(organ_id: params[:organ_id])
-        result=[]
-        actors.each do |actor|
-          result = result + actor.users
-        end
-        result
+        organ = Organ.find(params[:organ_id])
+        result = organ.members
       end
       return result
     end
