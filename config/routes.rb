b36e43f32c9eb6537332a3c03fa87c79a189588e
Tianbymy <xiegang@zhiyisoft.com>
2013-09-29 15:40:39 +0800

    routes调整

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,44 @@
-  # -*- coding: utf-8 -*-
-  EPBook::Application.routes.draw do
-    root :to => 'users/sessions#login'
+# -*- coding: utf-8 -*-
+EPBook::Application.routes.draw do
+    root :to => "users/sessions#login"
+  # public
+  resources :downloads, :only => [:index]
+  resources :news, :only => [:index]
+  resources :helps, :only => [:index]
 
-    # public
-    resources :downloads, :only => [:index]
-    resources :news, :only => [:index]
-    resources :helps, :only => [:index]
+  # devise
+  devise_for :users, :controllers => {
+    :registrations => "users/registrations",
+    :sessions => "users/sessions"
+  }
 
-    # devise
-    devise_for :users, :controllers => {
-      :registrations => "users/registrations",
-      :sessions => "users/sessions"
-    }
+  resources :dashboards, :only => [:index]
 
-    resources :dashboards, :only => [:index]
-
-    devise_scope :user do
-      get "/users/login" => "users/sessions#login"
-      get "/users/logout" => "users/sessions#logout"
-    end
-
-    resources :users do
-      get 'resend_email_confirmation', :on => :collection
-      member do
-        get 'adjust'
-        put 'adjust_post'
-      end
-    end
-
-    resources :organs, :except => [:destroy] do
-      member do
-        get 'remove_member'
-      end
-      collection do
-        get 'new_member'
-        post 'create_member'
-        get 'search'
-      end
-    end
-
-    resources :memberships, :except => [:show]
-
-    mount API => 'api'
+  devise_scope :user do
+    get "/users/login" => "users/sessions#login"
+    get "/users/logout" => "users/sessions#logout"
   end
+
+  resources :users do
+    get 'resend_email_confirmation', :on => :collection
+    member do
+      get 'adjust'
+      put 'adjust_post'
+    end
+  end
+
+  resources :organs, :except => [:destroy] do
+    member do
+      get 'remove_member'
+    end
+    collection do
+      get 'new_member'
+      post 'create_member'
+      get 'search'
+    end
+  end
+
+  resources :memberships, :except => [:show]
+  # api
+  mount API => 'api'
+end
