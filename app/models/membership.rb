a179d98fdcdd6a47b8bd6dd384be0ac977e06f86
Tang Jiujun <1549907179@qq.com>
2013-10-10 15:50:59 +0800

    系统初始化角色不能修改

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,9 +2,14 @@
 class Membership < ActiveRecord::Base
   attr_accessible :name
 
-  validates :name, :presence => true
+  validates :name, :presence => true, :uniqueness => true
 
   has_many :actors
 
-  before_destroy { actors.blank? }
+  before_update :not_system_membership?
+  before_destroy { not_system_membership? && actors.blank? }
+
+  def not_system_membership?
+    not Settings.memberships.include?(self.name)
+  end
 end
