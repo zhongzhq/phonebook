cd1dd11eb73ac8bb530353e190536831a9aecc3f
Tang Jiujun <1549907179@qq.com>
2013-10-11 00:16:12 +0800

    整理js， 组织管理中添加返回按钮

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# -*- coding: utf-8 -*-
 module ApplicationHelper
+  def input form, field_name, args = {}
+    form.input field_name, {:input_html => {:class => 'form-control'}}.merge(args)
+  end
+
+  def organ_back_path organ
+    organ.id.present? ? organ_path(organ) : (
+      organ.parent_id.present? ? organ_path( Organ.find(organ.parent_id) ) : root_path
+      )
+  end
 end
