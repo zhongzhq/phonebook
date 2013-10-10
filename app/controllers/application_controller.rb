a816388b0b9a9753abd809691d057fd675f13f6b
Tang Jiujun <1549907179@qq.com>
2013-10-10 17:04:10 +0800

    判断是否 xhr 请求，并设置 layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   has_widgets do |root|
-    root << widget('tree', :organ_tree)
+    root << widget("tree", :organ_tree)
   end
 
   include Phonebook::Controllers::Helpers
+
+  layout :set_layout
+  def set_layout
+    request.xhr? ? false : "application"
+  end
 end
