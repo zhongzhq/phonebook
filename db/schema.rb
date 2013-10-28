85b864d0a252b4152ee8efbd018e8a17a3f79b40
Tang Jiujun <1549907179@qq.com>
2013-10-28 15:25:21 +0800

    页面显示调整

--- db/schema.rb
+++ db/schema.rb
@@ -99,10 +99,4 @@ ActiveRecord::Schema.define(:version => 20131025065032) do
     t.datetime "updated_at",           :null => false
   end
 
-  create_table "ussdsfers", :force => true do |t|
-    t.text     "desc"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
 end
