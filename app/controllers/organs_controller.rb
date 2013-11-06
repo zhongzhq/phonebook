56cd55dc238389dc5f8bce6d7f8c3a1f13c07425
Tang Jiujun <1549907179@qq.com>
2013-11-06 15:23:19 +0800

    组织用户查询

--- app/controllers/organs_controller.rb
+++ app/controllers/organs_controller.rb
@@ -52,4 +52,29 @@ class OrgansController < ApplicationController
     @organ = Organ.find(params[:id])
     redirect_to manage_organs_path, (@organ.destroy ? {:notice => "组织删除成功"} : {:alert => "组织下还有成员或下级组织"})
   end
+
+  def search
+    organ = Organ.find(params[:id])
+    @value = params[:value]
+
+    param_one, param_two = params[:value].split(" ").map do |x|
+      x.downcase.split('').join("%").insert(0, "%").insert(-1, "%")
+    end
+
+    m = Member.where{organ_id.in organ.subtree.map(&:id)}
+
+    if @value.split.size == 1
+      # 用户 或 办公地址 或 电话
+      @users = User.joins(:members).where{ members.id.in m }.where{ (name.like param_one) | (pinyin.like param_one) }
+
+      # 职务
+      m1 = m.joins(:jobs).where{
+        (jobs.name.like param_one) | (jobs.pinyin.like param_one)
+      }
+      @users += User.joins(:members).where{ members.id.in m1 }
+    end
+
+    @users = @users || []
+    render "result"
+  end
 end
