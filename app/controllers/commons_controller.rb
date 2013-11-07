36eb34d4532bc5bd78cb89def15f4fa9e63ce604
Tang Jiujun <1549907179@qq.com>
2013-11-07 10:49:14 +0800

    常用电话代码优化

--- app/controllers/commons_controller.rb
+++ app/controllers/commons_controller.rb
@@ -1,42 +1,17 @@
 # -*- coding: utf-8 -*-
 class CommonsController < ApplicationController
   load_and_authorize_resource
-  
-  def index
-    @commons = Common.all
-  end
-
-  def new
-    @common = Common.new
-  end
 
   def create
-    @common = Common.new(params[:common])
-
-    if @common.save
-      redirect_to commons_path, :notice => "新建常用电话成功"
-    else
-      render 'new'
-    end
-  end
-
-  def edit
-    @common = Common.find(params[:id])
+    @common.save ? redirect_to(commons_path) : render("new")
   end
 
   def update
-    @common = Common.find(params[:id])
-
-    if @common.update_attributes(params[:common])
-      redirect_to commons_path, :notice => "更新常用电话成功"
-    else
-      render 'edit'
-    end
+    @common.update_attributes(params[:common]) ? redirect_to(commons_path) : render("edit")
   end
 
   def destroy
-    @common = Common.find(params[:id])
     @common.try(:destroy)
-    redirect_to commons_path, :notice => "删除常用电话成功"
+    redirect_to commons_path
   end
 end
