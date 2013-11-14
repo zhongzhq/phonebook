9f800d84cd3401e98d6fb085bd7fe3c02ec6a5a0
Tang Jiujun <1549907179@qq.com>
2013-11-14 15:31:19 +0800

    权限设置

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,15 +5,16 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    can [:index, :show, :search], Organ
     can [:change, :data_submit, :password_submit], User
-    can [:show, :with_organ], User
 
     user.members.each do |member|
       can :manage, :all if member.organ.parent.blank? && member.admin?
 
       if member.admin?
-        can [:new, :create], User
+        can :show, Organ do |organ|
+          user.admin_organ.subtree.include?(organ)
+        end
+        
         can :manage, User do |u|
           User.joins{members.organ}.where{members.organ.id.in member.organ.subtree.map(&:id)}.include?(u)
         end
